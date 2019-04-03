49f8a34a9ac7b950693543b185b533050c71e8ea
Pranav Anand <31237240+pranavroy@users.noreply.github.com>
2019-04-03 20:32:30 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+                <div class="clear"></div>
+            </div>
+            <!-- // #container -->
+        </div>	
+        <!-- // #containerHolder -->
+        
+        <p id="footer">Hospital Bed Management System. &copy; 2019. All Rights Reserved - Pranav Roy</p>
+    </div>
+    <!-- // #wrapper -->
+</body>
+</html>
